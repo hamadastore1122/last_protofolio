ed5bf519fabde8255c401a5d390ed04d85d7c697
Mohammed Yahya Abdelrahman shareef <142863486+hamadastore1122@users.noreply.github.com>
2024-09-20 19:56:28 +0300

    first commit

---
+++ assets/js/custm.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+function moveSlide(step) {
+    showSlide(currentSlide + step);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
